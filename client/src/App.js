3b9310ff1bf0f869759b01b63aa864d78ba27811
Saron <saronnhong@gmail.com>
2020-02-03 17:46:14 -0800

    add navbar and search bar with material

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav>
+          <div class="nav-wrapper light-blue darken-3">
+            <a href="#" class="brand-logo center "><i class="material-icons">face</i>Kraven's Last Hunt</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="sass.html">Sass</a></li>
+              <li><a href="badges.html">Components</a></li>
+              <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+          </div>
+        </nav>
+        <nav class="container searchBar">
+          <div class="nav-wrapper">
+            <form>
+              <div class="input-field white">
+                <input id="search" type="search" required />
+                <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                <i class="material-icons">close</i>
+              </div>
+            </form>
+          </div>
+          <button class="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+        </nav>
+
       </div>
     );
   }
